bd1dff1921fb990a5b76655fcc3315b08b65672b
danielsimonfuentes <dasimonfuentes@gmail.com>
2024-03-05 11:08:16 +0100

    commit

--- AcademiaIdiomas/Controladores/ControladorUsuario.cs
+++ AcademiaIdiomas/Controladores/ControladorUsuario.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AcademiaIdiomas
 {
@@ -60,6 +61,61 @@ namespace AcademiaIdiomas
             catch (Exception e)
             {
                 Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+        public static void insertarUsuarioBBDD()
+        {
+            // Cadena de conexión a la base de datos
+            // Ver método construirCadenaConexión más arriba
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\daniel\\Source\\Repos\\ProyectoInterfaces\\AcademiaIdiomas\\Academia.mdf;Integrated Security=True";
+            // Query de inserción
+            string query = "INSERT INTO Usuarios (Usuario, Nombre, Apellido1, Apellido2, Dni, Domicilio, FechaNac, Contrasena, Admin) VALUES(@Usuario, @Nombre, @Apellido1, @Apellido2, @Dni, @Domicilio, @FechaNac, @Contrasena, @Admin)";
+            // Valores para los parámetros
+            string descripcion = descripBox.Text;
+            DateTime fechaInicio = fecIniDateTimePicker.Value;
+            DateTime fechaFin = fecFinDateTimePicker.Value;
+            string estado = "false";
+            if (estadoCheckBox.Checked)
+            {
+                estado = "true";
+            }
+            double presupuestoInicial = Convert.ToDouble(preIniNumericUpDown.Text);
+            double presupuestoFinal = Convert.ToDouble(preFinNumericUpDown.Text);
+            string cambios = Convert.ToString(cambiosTextBox.Text);
+            string codigoCliente = codCliNumericUpDown.Text;
+            // Crear la conexión
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Abrir la conexión
+                connection.Open();
+                // Crear un objeto SqlCommand con la consulta y la conexión
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Agregar parámetros y sus valores
+                    // No se añade a la inserción el campo código proyecto porque es autonumérico, aunque se puede configurar para poder
+                    // insertarlo a la fuerza.
+                    command.Parameters.AddWithValue("@Descripcion", descripcion);
+                    command.Parameters.AddWithValue("@FechaInicio", fechaInicio);
+                    command.Parameters.AddWithValue("@FechaFin", fechaFin);
+                    command.Parameters.AddWithValue("@Estado", estado);
+                    command.Parameters.AddWithValue("@PresupuestoInicial",
+                    presupuestoInicial);
+                    command.Parameters.AddWithValue("@PresupuestoFinal",
+                    presupuestoFinal);
+                    command.Parameters.AddWithValue("@Cambios", cambios);
+                    command.Parameters.AddWithValue("@CodigoCliente", codigoCliente);
+                    try
+                    {
+                        // Ejecutar la consulta de inserción
+                        int registrosAfectados = command.ExecuteNonQuery();
+                        MessageBox.Show($"Se insertó correctamente el registro. Registros afectados: {registrosAfectados}");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al insertar el registro: {ex.Message}");
+                    }
+                }
             }
         }
     }
